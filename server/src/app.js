1353562de8f57905fb6f22a0fef67424cec738f7
codebydeep <deepanshvish123@gmail.com>
2026-03-25 20:55:25 +0530

    chore: fixes solved

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,14 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
 
 import healthCheckRoutes from "./routes/healthcheck.routes.js"
 import authRoutes from "./routes/auth.routes.js"
 
-const app = express()
+dotenv.config({ path: "./.env" })
 
-app.set("trust proxy", 1)
+const app = express()
 
 const allowedOrigins = (process.env.CORS_ORIGIN || "")
     .split(",")
@@ -15,15 +16,16 @@ const allowedOrigins = (process.env.CORS_ORIGIN || "")
     .filter(Boolean)
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            return callback(null, true)
-        }
-        return callback(new Error("Not allowed by CORS"))
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }))
 
 app.use(express.json())
